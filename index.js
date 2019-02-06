e1c09faed444b49d74614e80ad3fc2a117e0323c
David Stewart <davidstewart@gmail.com>
2019-02-06 02:41:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ function createRock(x) {
   function removeRock() {
   var rock = document.getElementsByClassName("rock");
   for (var i = 0; i < rock.length; i++) {
-    rock[0].remove()
+    rock[i].remove()
   }
   }
 
